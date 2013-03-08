503ae3fa08e84c2a805157bad34c308d52ca1d4d
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-03-08 10:56:00 +0100

    Add basic cabal files

--- src/NLP/Nerf/Types.hs
+++ src/NLP/Nerf/Types.hs
@@ -1,5 +1,31 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+-- | Basic Nerf internal types.
+
 module NLP.Nerf.Types
-(
+( O (..)
+, N (..)
+, T (..)
 ) where
 
+import Data.Vector.Generic.Base
+import Data.Vector.Generic.Mutable
+import qualified Data.Vector.Unboxed as U
 
+import Data.Binary (Binary)
+import Data.Vector.Binary ()
+
+-- | An observation.
+newtype O = O { unO :: Int }
+    deriving ( Show, Read, Eq, Ord, Binary
+             , Vector U.Vector, MVector U.MVector, U.Unbox )
+
+-- | A non-terminal symbol.
+newtype N = N { unN :: Int }
+    deriving ( Show, Read, Eq, Ord, Binary
+             , Vector U.Vector, MVector U.MVector, U.Unbox )
+
+-- | A terminal symbol.
+newtype T = T { unT :: Int }
+    deriving ( Show, Read, Eq, Ord, Binary
+             , Vector U.Vector, MVector U.MVector, U.Unbox )
